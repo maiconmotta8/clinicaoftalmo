1e5456dc53870acb2eb21cc49a5d39d13dfcca61
Maicon da Silva Motta <maiconmotta8@gmail.com>
2018-07-06 21:16:03 -0300

    commit teste do login

--- Teste_Clinica/src/Login.java
+++ Teste_Clinica/src/Login.java
@@ -1,7 +1,10 @@
 
 
+import javax.swing.JOptionPane;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Login {
@@ -14,12 +17,27 @@ public class Login {
 		 System.setProperty("webdriver.gecko.driver","C:\\uc13\\geckodriver.exe");
 			
 			pagina = new FirefoxDriver();
-			pagina.get("https://www.senacrs.com.br/");
-			
+			pagina.get("http://10.10.139.13/clinicaOftalmo/");
+			pagina.manage().window().maximize();
 	}
 
 	@Test
 	public void test() {
+		
+
+		WebElement email_Cpf = pagina.findElementByName("login");
+		WebElement senha = pagina.findElementByName("senha");
+		WebElement entrar= pagina.findElementByXPath("/html/body/header/form/input[3]");
+		
+		
+		
+		email_Cpf.sendKeys("xeroxdojapones@gmail.com");
+		senha.sendKeys("123");
+		entrar.click();
+		
+		
+		JOptionPane.showMessageDialog(null,  "TESTE OK");
+	
 		
 	}
 
